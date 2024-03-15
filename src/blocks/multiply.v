5868904b8f88a002875d23fa478b8716bd5b69fa
Adelin Igas <adelin.igas@student.upt.ro>
2024-03-15 16:01:15 +0200

    Multiply and sum Updated

--- src/blocks/multiply.v
+++ src/blocks/multiply.v
@@ -1,7 +1,7 @@
 module multiply_block (
 	input [31:0] a,
 	input [31:0] b,
-	output [32:0] out
+	output [63:0] out
 );
 
 	assign out = a * b;
--- src/blocks/sum.v
+++ src/blocks/sum.v
@@ -1,7 +1,7 @@
 module sum_block (
 	input [31:0] a,
 	input [31:0] b,
-	output [31:0] out
+	output [32:0] out
 );
 
 	assign out = a + b;
